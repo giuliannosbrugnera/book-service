a7247c79f7e714e5f3474cdbdba15de0f98edfc3
Giulianno Sbrugnera <giulianno.sbrugnera@gmail.com>
2016-08-15 17:32:57 -0300

    Add a new iem to the database

--- BookService/Scripts/app.js
+++ BookService/Scripts/app.js
@@ -3,7 +3,16 @@
     self.books = ko.observableArray();
     self.error = ko.observable();
     self.detail = ko.observable();
+    self.authors = ko.observableArray();
+    self.newBook = {
+        Author: ko.observable(),
+        Genre: ko.observable(),
+        Price: ko.observable(),
+        Title: ko.observable(),
+        Year: ko.observable()
+    }
 
+    var authorsUri = '/api/authors/';
     var booksUri = '/api/books/';
 
     function ajaxHelper(uri, method, data) {
@@ -25,14 +34,37 @@
         });
     }
 
+    function getAuthors() {
+        ajaxHelper(authorsUri, 'GET').done(function (data) {
+            self.authors(data);
+        });
+    }
+
     self.getBookDetail = function (item) {
         ajaxHelper(booksUri + item.Id, 'GET').done(function (data) {
             self.detail(data);
         });
     }
 
+    self.addBook = function (formElement) {
+        var book = {
+            AuthorId: self.newBook.Author().Id,
+            Genre: self.newBook.Genre(),
+            Price: self.newBook.Price(),
+            Title: self.newBook.Title(),
+            Year: self.newBook.Year()
+        };
+
+        ajaxHelper(booksUri, 'POST', book).done(function (item) {
+            self.books.push(item);
+            $('#bookSuccessModal').modal('show');
+            $('#addBookForm')[0].reset();
+        });
+    }
+
     // Fetch the initial data.
     getAllBooks();
+    getAuthors();
 };
 
 ko.applyBindings(new ViewModel());
